6452cde8d5a8ccd2bc09df78d1dca26fc1585ea0
MelnCat <melncatuwu@gmail.com>
2024-07-20 13:33:41 -0700

    update

--- src/app/rooms/RoomListItem.tsx
+++ src/app/rooms/RoomListItem.tsx
@@ -6,8 +6,19 @@ import styles from "./rooms.module.scss";
 export const RoomListItem = ({ room, canJoin }: { room: RoomListData | null; canJoin: boolean }) => {
 	return (
 		<article className={styles.room}>
-			<h1>{room?.name ?? <Skeleton />}</h1>
-			<p>{room !== null ? `Player Count: ${room.playerCount}/${room.max}` : <Skeleton />}</p>
+			<div className={styles.roomHeader}>
+				<h1>{room?.name ?? <Skeleton />}</h1>
+				<p className={styles.roomPlayers}>
+					{room !== null ? (
+						<>
+							{room.playerCount}/{room.max}
+						</>
+					) : (
+						<Skeleton />
+					)}
+				</p>
+			</div>
+			<p className={styles.roomStatus}>{room !== null ? room.state === "lobby" ? "In Lobby" : "In Game" : <Skeleton />}</p>
 			<p>{room !== null ? `Owner: ${room.owner}` : <Skeleton />}</p>
 			{canJoin && room !== null && <button onClick={() => socket.emit("room:join", room.name)}>Join</button>}
 		</article>
--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -89,53 +89,55 @@ export default function RoomsPage() {
 						<header>
 							<h2>Create New Room</h2>
 						</header>
-						<input className={styles.createInput} placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
-						<div>
-							Deck:{" "}
-							<select value={option} onChange={x => setOption(x.target.value)}>
-								{Object.keys(deckTypes).map(x => (
-									<option key={x} value={x}>
-										{x}
-									</option>
+						<section className={styles.createOptions}>
+							<input className={styles.createInput} placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+							<div>
+								Deck:{" "}
+								<select value={option} onChange={x => setOption(x.target.value)}>
+									{Object.keys(deckTypes).map(x => (
+										<option key={x} value={x}>
+											{x}
+										</option>
+									))}
+								</select>
+							</div>
+							<div>
+								{Object.entries(rules).map(([key, value]) => (
+									<div key={key}>
+										{key}:
+										<input value={value} onChange={x => setRules({ ...rules, [key]: x.target.value })} />
+									</div>
 								))}
-							</select>
-						</div>
-						<div>
-							{Object.entries(rules).map(([key, value]) => (
-								<div key={key}>
-									{key}:
-									<input value={value} onChange={x => setRules({ ...rules, [key]: x.target.value })} />
-								</div>
-							))}
-						</div>
-						<div>
-							Late Joins: <input type="checkbox" value={lateJoins ? "on" : ""} onChange={x => setLateJoins(x => !x)} />
-						</div>
-						<div>
-							Max Players: <input type="number" value={max} onChange={x => setMax(+x.target.value)} />
-						</div>
-						<button
-							className={styles.createButton}
-							onClick={() =>
-								name.trim() &&
-								socket.emit(
-									"room:create",
-									{
-										name,
-										unlisted: false,
-										deckType: option,
-										rules: Object.fromEntries(Object.entries(rules).map(x => [x[0], JSON.parse(x[1])])) as GameRules,
-										lateJoins,
-										max,
-									},
-									() => {
-										router.push("/room");
-									}
-								)
-							}
-						>
-							Create
-						</button>
+							</div>
+							<div>
+								Late Joins: <input type="checkbox" value={lateJoins ? "on" : ""} onChange={x => setLateJoins(x => !x)} />
+							</div>
+							<div>
+								Max Players: <input type="number" value={max} onChange={x => setMax(+x.target.value)} />
+							</div>
+							<button
+								className={styles.createButton}
+								onClick={() =>
+									name.trim() &&
+									socket.emit(
+										"room:create",
+										{
+											name,
+											unlisted: false,
+											deckType: option,
+											rules: Object.fromEntries(Object.entries(rules).map(x => [x[0], JSON.parse(x[1])])) as GameRules,
+											lateJoins,
+											max,
+										},
+										() => {
+											router.push("/room");
+										}
+									)
+								}
+							>
+								Create
+							</button>
+						</section>
 					</section>
 				</article>
 			</article>
